ccefc2b761091a56066b7c5ee3c3af174f58b9d8
jack <jack@is-land.com.tw>
2015-12-09 09:57:50 +0800

    create varone standalone project

---
+++ varOne-exec/src/main/java/com/varone/exec/TomcatServer.java
@@ -0,0 +1,22 @@
+package com.varone.exec;
+//import java.net.URL;
+
+//import org.apache.catalina.startup.Tomcat;
+public class TomcatServer {
+	public static void main(String args[]){
+			System.out.println("aaaaaaa");
+			System.out.println("bbbbbbb");
+			System.out.println("ccccccc");
+		   /* Tomcat tomcat = new Tomcat();
+		
+			tomcat.setPort(8888);
+			tomcat.setBaseDir("/tmp");
+			tomcat.addWebapp("/varOne", "/home/user1/WebContainer.war");		
+			tomcat.start();
+			tomcat.getServer().await();
+			*/
+			//URL url = TomcatServer.class.getProtectionDomain().getCodeSource().getLocation();
+			//url.get
+	}
+	
+}
