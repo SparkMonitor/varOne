d78ff8f956a7ef687a02b128bf620dadf4be614f
AllenFang <ayu780129@hotmail.com>
2015-12-16 14:21:56 +0800

    Frontend timeperiod implmentation with job dashboard

--- varOne-web/src/main/webapp/app/actions/job-action.js
+++ varOne-web/src/main/webapp/app/actions/job-action.js
@@ -2,16 +2,16 @@ import alt from '../alt';
 import request from 'superagent';
 
 class JobAction {
-  async fetchJobDashBoard(appId, selectMetrics){
+  async fetchJobDashBoard(appId, selectMetrics, period){
 
     if(selectMetrics && selectMetrics.length == 0) selectMetrics = undefined;
     else selectMetrics = selectMetrics.join(",");
 
     let response = await request.get('/varOne-web/rest/job/'+appId)
-                                .query({metrics: selectMetrics})
+                                .query({metrics: selectMetrics, period: period})
                                 .set('Accept', 'application/json');
     let result = JSON.parse(response.text);
-    this.dispatch(result);
+    this.dispatch({ result, period });
   }
 }
 
--- varOne-web/src/main/webapp/app/components/job/job-header.js
+++ varOne-web/src/main/webapp/app/components/job/job-header.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import TimePeriodPill from '../commons/time-period-pills'
 
 class JobHeader extends React.Component {
 
   static propTypes = {
-    appId: React.PropTypes.string
+    appId: React.PropTypes.string,
+    period: React.PropTypes.string,
+    onPeriodSelect: React.PropTypes.func
   }
 
   render(){
     return(
       <div className="row">
-          <div className="col-lg-12">
-              <h1 className="page-header">{this.props.appId}&nbsp;&nbsp;
-                <i className="fa fa-x fa-bar-chart" data-toggle="modal" data-target="#metricsModal"></i></h1>
+          <div className="col-md-4">
+              <h3 className="page-header">{this.props.appId}&nbsp;&nbsp;
+                <i className="fa fa-x fa-bar-chart" data-toggle="modal" data-target="#metricsModal"></i></h3>
+          </div>
+          <div className="col-md-5">
+              <h3 className="page-header">
+                <TimePeriodPill active={this.props.period} onPeriodSelect={this.props.onPeriodSelect}/>
+              </h3>
           </div>
       </div>
     );
