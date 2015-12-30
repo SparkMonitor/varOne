edf9d54643a8e4eaa791000d12e086312c061f2b
jack <jack@is-land.com.tw>
2015-12-30 11:49:10 +0800

    optimize js for production

--- varOne-web/src/main/webapp/webpack/production-config.js
+++ varOne-web/src/main/webapp/webpack/production-config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require('webpack');
+
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
 
 var config = {
@@ -15,6 +17,34 @@ var config = {
         {test: /\.css$/, loader: 'style-loader!css-loader'}
       ]
     },
+    plugins: [
+		//optimizations
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+		  compress: {
+		    warnings: false,
+		    screw_ie8: true,
+		    sequences: true,
+		    dead_code: true,
+		    drop_debugger: true,
+		    comparisons: true,
+		    conditionals: true,
+		    evaluate: true,
+		    booleans: true,
+		    loops: true,
+		    unused: true,
+		    hoist_funs: true,
+		    if_return: true,
+		    join_vars: true,
+		    cascade: true,
+		    drop_console: true
+		  },
+		  output: {
+		    comments: false
+		  }
+		})
+    ],
     resolve: {
       extensions: ['', '.js', '.jsx'],
       // root: [path.join(__dirname, "public", "javascripts")],
