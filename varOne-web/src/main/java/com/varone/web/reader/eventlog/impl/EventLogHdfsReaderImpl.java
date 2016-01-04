3d7899619a5039425a9fedf38cede633e0107ff0
jack <jack@is-land.com.tw>
2016-01-04 14:48:17 +0800

    try-catch write error log

--- varOne-web/src/main/java/com/varone/web/reader/eventlog/impl/EventLogHdfsReaderImpl.java
+++ varOne-web/src/main/java/com/varone/web/reader/eventlog/impl/EventLogHdfsReaderImpl.java
@@ -50,8 +50,7 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 	 * @see com.varone.web.reader.eventlog.EventLogReader#getAllInProgressLog()
 	 */
 	@Override
-	public Map<String, SparkEventLogBean> getAllInProgressLog()
-			throws Exception {
+	public Map<String, SparkEventLogBean> getAllInProgressLog() {
 		logger.info("getAllInProgressLog method");
 		
 		Map<String, SparkEventLogBean> allEventLogs = new LinkedHashMap<String, SparkEventLogBean>();
@@ -62,29 +61,45 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		    	return path.getName().endsWith(".inprogress");
 		    }
 		};
-		FileStatus[] allInProgressFileState = fs.listStatus(this.logDir, filter);
-				
-		for(FileStatus status: allInProgressFileState){
-			SparkEventLogParser parser = new SparkEventLogParser();
-			FSDataInputStream fis = fs.open(status.getPath());
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            String line;
-            while(null != (line=br.readLine())){
-            	parser.parseLine(line);
-            }
-            br.close();
-            allEventLogs.put(status.getPath().getName(), parser.result());
+		try {
+			FileStatus[] allInProgressFileState = fs.listStatus(this.logDir, filter);
+					
+			for(FileStatus status: allInProgressFileState){
+				BufferedReader br = null;
+				SparkEventLogParser parser = new SparkEventLogParser();
+				try {
+					FSDataInputStream fis = fs.open(status.getPath());
+				    br = new BufferedReader(new InputStreamReader(fis));
+		            String line;
+		            while(null != (line=br.readLine())){
+		            	parser.parseLine(line);
+		            }
+				}catch(Exception e){
+					logger.error(e);
+					throw new RuntimeException(e);
+				}finally{
+					try{
+						br.close();
+					}catch(Exception e){
+						logger.error(e);
+						throw new RuntimeException(e);
+					}
+				}
+	            allEventLogs.put(status.getPath().getName(), parser.result());
+			}
+			
+			return allEventLogs;
+		}catch(Exception e){
+			logger.error(e);
+			throw new RuntimeException(e);
 		}
-		
-		return allEventLogs;
 	}
 
 	/* (non-Javadoc)
 	 * @see com.varone.web.reader.eventlog.EventLogReader#getInProgressLog(java.lang.String)
 	 */
 	@Override
-	public SparkEventLogBean getInProgressLog(final String applicationId)
-			throws Exception {
+	public SparkEventLogBean getInProgressLog(final String applicationId) {
 		logger.info("getInProgressLog method, applicationId = " + applicationId);
 		
 		PathFilter filter = new PathFilter(){
@@ -100,22 +115,33 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		eventNames.add(SparkEventLogParser.JOB_START);
 		eventNames.add(SparkEventLogParser.TASK_START);
 		eventNames.add(SparkEventLogParser.TASK_END);
-		FileStatus[] allInProgressFileState = fs.listStatus(this.logDir, filter);
-		
-		FileStatus status = allInProgressFileState[0];
 		SparkEventLogParser parser = new SparkEventLogParser(eventNames);
-		FSDataInputStream fis = fs.open(status.getPath());
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-        String line;
-        while(null != (line=br.readLine())){
-        	parser.parseLine(line);
-        }
-        br.close();
+		BufferedReader br = null;
+		try {
+			FileStatus[] allInProgressFileState = fs.listStatus(this.logDir, filter);
+			FileStatus status = allInProgressFileState[0];
+			
+			FSDataInputStream fis = fs.open(status.getPath());
+			br = new BufferedReader(new InputStreamReader(fis));
+	        String line;
+	        while(null != (line=br.readLine())){
+	        	parser.parseLine(line);
+	        }
+		}catch(Exception e){
+			logger.error(e);
+			throw new RuntimeException(e);
+		}finally{
+			try {
+				br.close();
+			}catch(Exception e){
+				logger.error(e);
+				throw new RuntimeException(e);
+			}
+		}
 		return parser.result();
 	}
 	
-	public SparkEventLogBean getHistoryStageDetails(final String applicationId) 
-			throws Exception{
+	public SparkEventLogBean getHistoryStageDetails(final String applicationId) {
 		logger.info("getHistoryStageDetails method, applicationId = " + applicationId);
 		PathFilter filter = new PathFilter() {
 			@Override
@@ -136,22 +162,35 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		eventNames.add(SparkEventLogParser.STAGE_SUBMIT);
 		eventNames.add(SparkEventLogParser.STAGE_COMPLETED);
 		eventNames.add(SparkEventLogParser.BLOCKMANAGER_ADD);
-		
-		FileStatus[] applicationHistoryFileState = fs.listStatus(this.logDir, filter);
-		FileStatus status = applicationHistoryFileState[0];
 		SparkEventLogParser parser = new SparkEventLogParser(eventNames);
-		FSDataInputStream fis = fs.open(status.getPath());
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-		String line;
-		while(null != (line=br.readLine())){
-			parser.parseLine(line);
+		BufferedReader br = null;
+		try {
+			FileStatus[] applicationHistoryFileState = fs.listStatus(this.logDir, filter);
+			FileStatus status = applicationHistoryFileState[0];
+
+			FSDataInputStream fis = fs.open(status.getPath());
+			br = new BufferedReader(new InputStreamReader(fis));
+			String line;
+			while(null != (line=br.readLine())){
+				parser.parseLine(line);
+			}
+		} catch(Exception e) {
+			logger.error(e);
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				br.close();
+			} catch(Exception e){
+				logger.error(e);
+				throw new RuntimeException(e);
+			}
 		}
-		br.close();
+	
 		return parser.result();
 	}
 
 	@Override
-	public List<SparkEventLogBean> getAllSparkAppLog() throws Exception {
+	public List<SparkEventLogBean> getAllSparkAppLog() {
 		logger.info("getAllSparkAppLog method");
 		List<SparkEventLogBean> result = new ArrayList<SparkEventLogBean>();
 		
@@ -161,35 +200,45 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		    	return !path.getName().endsWith(".inprogress");
 		    }
 		};
-		
-		FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
-		
-		List<String> eventNames = new ArrayList<String>();
-		eventNames.add(SparkEventLogParser.APP_START);
-		eventNames.add(SparkEventLogParser.APP_END);
-		
-		for(FileStatus status: allFileState){
-			SparkEventLogParser parser = new SparkEventLogParser(eventNames);
-			parser.enableBreakPoint();
-			FSDataInputStream fis = fs.open(status.getPath());
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            String line = null, tmp;
-            boolean found = false;
-            while(null != (tmp=br.readLine())){
-            	line = tmp;
-            	if(!found)
-            		found = parser.parseLine(line);
-            }
-            parser.parseLine(line);
-            br.close();
-            result.add(parser.result());
+		try{
+			FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
+			
+			List<String> eventNames = new ArrayList<String>();
+			eventNames.add(SparkEventLogParser.APP_START);
+			eventNames.add(SparkEventLogParser.APP_END);
+			
+			for(FileStatus status: allFileState){
+				SparkEventLogParser parser = new SparkEventLogParser(eventNames);
+				parser.enableBreakPoint();
+				BufferedReader br = null;
+				try{
+					FSDataInputStream fis = fs.open(status.getPath());
+					br = new BufferedReader(new InputStreamReader(fis));
+		            String line = null, tmp;
+		            boolean found = false;
+		            while(null != (tmp=br.readLine())){
+		            	line = tmp;
+		            	if(!found)
+		            		found = parser.parseLine(line);
+		            }
+		            parser.parseLine(line);
+				}catch(Exception e){
+					logger.error(e);
+					throw new RuntimeException(e);
+				}finally{
+					br.close();
+				}
+	            result.add(parser.result());
+			}
+		}catch(Exception e){
+			logger.error(e);
+			throw new RuntimeException(e);
 		}
 		return result;
 	}
 
 	@Override
-	public SparkEventLogBean getApplicationJobs(final String applicationId)
-			throws Exception {
+	public SparkEventLogBean getApplicationJobs(final String applicationId) {
 		logger.info("getApplicationJobs method, applicationId = " + applicationId);
 		PathFilter filter = new PathFilter(){
 			@Override
@@ -198,31 +247,42 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		    			path.getName().indexOf(applicationId) != -1;
 		    }
 		};
-		
-		FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
-		
-		List<String> eventNames = new ArrayList<String>();
-		eventNames.add(SparkEventLogParser.JOB_START);
-		eventNames.add(SparkEventLogParser.JOB_END);
-		eventNames.add(SparkEventLogParser.TASK_END);
-		eventNames.add(SparkEventLogParser.STAGE_SUBMIT);
-		eventNames.add(SparkEventLogParser.STAGE_COMPLETED);
-		
-		FileStatus status = allFileState[0];
-		SparkEventLogParser parser = new SparkEventLogParser(eventNames);
-		FSDataInputStream fis = fs.open(status.getPath());
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-        String line;
-        while(null != (line=br.readLine())){
-        	parser.parseLine(line);
-        }
-        br.close();
+		SparkEventLogParser parser = null;
+		BufferedReader br = null;
+		try {
+			FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
+			
+			List<String> eventNames = new ArrayList<String>();
+			eventNames.add(SparkEventLogParser.JOB_START);
+			eventNames.add(SparkEventLogParser.JOB_END);
+			eventNames.add(SparkEventLogParser.TASK_END);
+			eventNames.add(SparkEventLogParser.STAGE_SUBMIT);
+			eventNames.add(SparkEventLogParser.STAGE_COMPLETED);
+			
+			FileStatus status = allFileState[0];
+			parser = new SparkEventLogParser(eventNames);
+			FSDataInputStream fis = fs.open(status.getPath());
+			br = new BufferedReader(new InputStreamReader(fis));
+	        String line;
+	        while(null != (line=br.readLine())){
+	        	parser.parseLine(line);
+	        }
+		} catch(Exception e) {
+			logger.error(e);
+			throw new RuntimeException(e);
+		}finally {
+			try {
+				br.close();
+			}catch(Exception e){
+				logger.error(e);
+				throw new RuntimeException(e);
+			}
+		}
 		return parser.result();
 	}
 
 	@Override
-	public SparkEventLogBean getJobStages(final String applicationId, String jobId)
-			throws Exception {
+	public SparkEventLogBean getJobStages(final String applicationId, String jobId) {
 		logger.info("getJobStages method, applicationId = " + applicationId + " jobId = " + jobId);
 		
 		PathFilter filter = new PathFilter(){
@@ -232,23 +292,35 @@ public class EventLogHdfsReaderImpl implements EventLogReader {
 		    			path.getName().indexOf(applicationId) != -1;
 		    }
 		};
-		
-		FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
-		List<String> eventNames = new ArrayList<String>();
-		eventNames.add(SparkEventLogParser.JOB_START);
-		eventNames.add(SparkEventLogParser.STAGE_SUBMIT);
-		eventNames.add(SparkEventLogParser.STAGE_COMPLETED);
-		eventNames.add(SparkEventLogParser.TASK_END);
-		
-		FileStatus status = allFileState[0];
-		SparkEventLogParser parser = new SparkEventLogParser(eventNames);
-		FSDataInputStream fis = fs.open(status.getPath());
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-        String line;
-        while(null != (line=br.readLine())){
-        	parser.parseLine(line);
-        }
-        br.close();
+		SparkEventLogParser parser = null;
+		BufferedReader br = null;
+		try {
+			FileStatus[] allFileState = fs.listStatus(this.logDir, filter);
+			List<String> eventNames = new ArrayList<String>();
+			eventNames.add(SparkEventLogParser.JOB_START);
+			eventNames.add(SparkEventLogParser.STAGE_SUBMIT);
+			eventNames.add(SparkEventLogParser.STAGE_COMPLETED);
+			eventNames.add(SparkEventLogParser.TASK_END);
+			
+			FileStatus status = allFileState[0];
+			parser = new SparkEventLogParser(eventNames);
+			FSDataInputStream fis = fs.open(status.getPath());
+			br = new BufferedReader(new InputStreamReader(fis));
+	        String line;
+	        while(null != (line=br.readLine())){
+	        	parser.parseLine(line);
+	        }
+		} catch(Exception e){
+			logger.error(e);
+			throw new RuntimeException(e);
+		} finally {
+			try {
+			   br.close();
+			}catch(Exception e){
+				logger.error(e);
+				throw new RuntimeException(e);
+			}
+		}
 		return parser.result();
 	}
 
