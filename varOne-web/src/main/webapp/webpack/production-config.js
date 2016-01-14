734f793b66023805f197dba2fc26427bdc04bb85
AllenFang <ayu780129@hotmail.com>
2016-01-14 11:26:13 +0800

    make jquery loaded as module on production build

--- varOne-web/src/main/webapp/webpack/production-config.js
+++ varOne-web/src/main/webapp/webpack/production-config.js
@@ -16,6 +16,7 @@ var config = {
         { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/},
         { test: /\.scss$/, loaders: [ 'style', 'css', 'sass' ] },
         { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
+        { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
         {
           test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)(\?v=[0-9].[0-9].[0-9])?$/,
           loader: 'file?name=[sha512:hash:base64:7].[ext]',
@@ -26,6 +27,12 @@ var config = {
     },
     plugins: [
 		//optimizations
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        'root.jQuery': 'jquery'
+    }),
     new ExtractTextPlugin('app.css', { allChunks: true }),
 		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
