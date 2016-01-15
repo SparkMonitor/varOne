d50e0d6e0f74b581c77cf2ae0256968f9f46e39d
AllenFang <ayu780129@hotmail.com>
2016-01-15 14:27:01 +0800

    lint cluster compoents

--- varOne-web/src/main/webapp/app/components/cluster/cluster-summary.js
+++ varOne-web/src/main/webapp/app/components/cluster/cluster-summary.js
@@ -1,40 +1,40 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Const from '../../utils/consts';
 import ClusterSummaryBlock from './cluster-summary-block';
 
 class ClusterSummary extends React.Component {
 
-  handleSummaryBlockClick(title){
-    if(title === Const.node.summary.NODE_QTY){
+  static propTypes = {
+    summaryInfos: PropTypes.array,
+    onNodeClick: PropTypes.func
+  }
+
+  handleSummaryBlockClick = title => {
+    if (title === Const.node.summary.NODE_QTY) {
       this.props.onNodeClick();
     }
   }
 
-  render(){
-
-    let summaryContents = this.props.summaryInfos.map((summary) => {
-      return(
-        <ClusterSummaryBlock key={summary.displayName}
-                          title={summary.displayName}
-                          panel={summary.panel}
-                          icon={summary.icon}
-                          value={summary.value}
-                          onSummaryClick={(title) => this.handleSummaryBlockClick(title)}/>
+  render() {
+    const summaryContents = this.props.summaryInfos.map((summary) => {
+      return (
+        <ClusterSummaryBlock
+          key={ summary.displayName }
+          title={ summary.displayName }
+          panel={ summary.panel }
+          icon={ summary.icon }
+          value={ summary.value }
+          onSummaryClick={ this.handleSummaryBlockClick }/>
       );
     });
 
 
-    return(
-      <div classNameName="row">
-        {summaryContents}
+    return (
+      <div classNameName='row'>
+        { summaryContents }
       </div>
     );
   }
-}
-
-ClusterSummary.propTypes = {
-  summaryInfos: React.PropTypes.array,
-  onNodeClick: React.PropTypes.func
 }
 
 export default ClusterSummary;
