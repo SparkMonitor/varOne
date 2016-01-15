0b54ef227e537e01030313162651fe6b275a1e18
AllenFang <ayu780129@hotmail.com>
2016-01-15 14:26:30 +0800

    lint actions

--- varOne-web/src/main/webapp/app/actions/cluster-action.js
+++ varOne-web/src/main/webapp/app/actions/cluster-action.js
@@ -3,13 +3,13 @@ import request from 'superagent';
 
 class ClusterAction {
 
-  async fetchTotalNodeDashBoard(selectMetrics, period){
-    if(selectMetrics && selectMetrics.length == 0) selectMetrics = undefined;
-    else selectMetrics = selectMetrics.join(",");
-    let response = await request.get('/varOne-web/rest/cluster')
-                                .query({metrics: selectMetrics, period: period})
+  async fetchTotalNodeDashBoard(selectMetrics, period) {
+    if (selectMetrics && selectMetrics.length === 0) selectMetrics = undefined;
+    else selectMetrics = selectMetrics.join(',');
+    const response = await request.get('/varOne-web/rest/cluster')
+                                .query({ metrics: selectMetrics, period })
                                 .set('Accept', 'application/json');
-    let result = JSON.parse(response.text);
+    const result = JSON.parse(response.text);
     this.dispatch({ result, period });
   }
 
--- varOne-web/src/main/webapp/app/actions/menu-action.js
+++ varOne-web/src/main/webapp/app/actions/menu-action.js
@@ -2,10 +2,10 @@ import alt from '../alt';
 import request from 'superagent';
 class MenuAction {
 
-  async fetchRunningJob(){
-    let response = await request.get('/varOne-web/rest/job')
+  async fetchRunningJob() {
+    const response = await request.get('/varOne-web/rest/job')
                               .set('Accept', 'application/json');
-    let result = JSON.parse(response.text);
+    const result = JSON.parse(response.text);
     this.dispatch(result);
   }
 
