ab7e2387a7c448c76e43977d23fca6b49b0621a3
AllenFang <ayu780129@hotmail.com>
2016-01-15 16:10:53 +0800

    lint history components

--- varOne-web/src/main/webapp/app/components/history/history-header.js
+++ varOne-web/src/main/webapp/app/components/history/history-header.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class HistoryHeader extends React.Component {
 
-  render(){
-    var headerStyle = {
+  static propTypes = {
+    children: PropTypes.element
+  }
+
+  render() {
+    const headerStyle = {
       borderBottom: '1px solid #e7ecf1'
     };
 
     return (
-      <div style={headerStyle}>
-        {this.props.children}
+      <div style={ headerStyle }>
+        { this.props.children }
       </div>
     );
   }
