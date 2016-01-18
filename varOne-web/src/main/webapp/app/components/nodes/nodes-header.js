1158f026792a99d22619310b05fdd8254b255515
AllenFang <ayu780129@hotmail.com>
2016-01-18 11:03:08 +0800

    lint node components

--- varOne-web/src/main/webapp/app/components/nodes/nodes-header.js
+++ varOne-web/src/main/webapp/app/components/nodes/nodes-header.js
@@ -1,60 +1,68 @@
-import React from 'react';
-import TimePeriodPill from '../commons/time-period-pills'
+import React, { PropTypes } from 'react';
+import TimePeriodPill from '../commons/time-period-pills';
 
 class NodeHeader extends React.Component {
 
   static propTypes = {
-    nodes: React.PropTypes.array,
-    period: React.PropTypes.string,
-    onNodeSelect: React.PropTypes.func,
-    onPeriodSelect: React.PropTypes.func
+    nodes: PropTypes.array,
+    period: PropTypes.string,
+    onNodeSelect: PropTypes.func,
+    onPeriodSelect: PropTypes.func
   }
 
   state = {
     selected: 'Select Node'
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  handleNodeItemClick = (e) => {
+  handleNodeItemClick = e => {
     this.props.onNodeSelect(e.target.text);
     this.setState({
       selected: e.target.text
     });
   }
 
-  render(){
-
-    let nodes = this.props.nodes.map((node) => {
+  render() {
+    const nodes = this.props.nodes.map((node) => {
       return (
-        <li key={node} onClick={this.handleNodeItemClick}><a href="#">{node}</a></li>
+        <li key={ node } onClick={ this.handleNodeItemClick }><a href='#'>{ node }</a></li>
       );
     });
 
-    return(
-      <div className="row">
-          <div className="col-md-3">
-              <h2 className="page-header">
-                Nodes&nbsp;&nbsp;
-                <div className="btn-group">
-                  <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    {this.state.selected} <span className="caret"></span>
-                  </button>
-                  <ul className="dropdown-menu">
-                    {nodes}
-                  </ul>
-                  &nbsp;
-                  <i className="fa fa-x fa-pencil-square-o" data-toggle="modal" data-target="#metricsModal"></i>
-                </div>
-              </h2>
-          </div>
-          <div className="col-md-5">
-              <h4 className="page-header">
-                <TimePeriodPill active={this.props.period} onPeriodSelect={this.props.onPeriodSelect}/>
-              </h4>
-          </div>
+    return (
+      <div className='row'>
+        <div className='col-md-3'>
+          <h2 className='page-header'>
+            Nodes&nbsp;&nbsp;
+            <div className='btn-group'>
+              <button
+                type='button'
+                className='btn btn-default dropdown-toggle'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'>
+                { this.state.selected } <span className='caret'></span>
+              </button>
+              <ul className='dropdown-menu'>
+                { nodes }
+              </ul>
+              &nbsp;
+              <i className='fa fa-x fa-pencil-square-o'
+                data-toggle='modal'
+                data-target='#metricsModal'></i>
+            </div>
+          </h2>
+        </div>
+        <div className='col-md-5'>
+          <h4 className='page-header'>
+            <TimePeriodPill
+              active={ this.props.period }
+              onPeriodSelect={ this.props.onPeriodSelect }/>
+          </h4>
+        </div>
       </div>
     );
   }
--- varOne-web/src/main/webapp/app/components/nodes/nodes-metrics.js
+++ varOne-web/src/main/webapp/app/components/nodes/nodes-metrics.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import NodeMetric from './nodes-metric';
 
 class NodeMetrics extends React.Component {
 
+  static propTypes = {
+    metrics: PropTypes.array
+  }
 
-  render(){
-    var metricCharts = this.props.metrics.map((metric, i) => {
-      return(
-        <NodeMetric key={i} metric={metric}/>
+  render() {
+    const metricCharts = this.props.metrics.map((metric, i) => {
+      return (
+        <NodeMetric key={ i } metric={ metric }/>
       );
     });
 
-    return(
-      <div className="row">
-          <div className="col-lg-12">
-              {metricCharts}
-          </div>
+    return (
+      <div className='row'>
+        <div className='col-lg-12'>
+          { metricCharts }
+        </div>
       </div>
     );
   }
 }
-
 export default NodeMetrics;
