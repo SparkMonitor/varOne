d33e4fabd226636a6eec277c3822d0207579b802
jack <jack@is-land.com.tw>
2016-02-26 15:13:25 +0800

    Rename restful resource name

--- varOne-web/src/main/webapp/app/actions/varOne-action.js
+++ varOne-web/src/main/webapp/app/actions/varOne-action.js
@@ -9,7 +9,7 @@ class VarOneAction {
   }
 
   async fetchTimerInterval() {
-    const response = await request.get('/varOne-web/rest/timer')
+    const response = await request.get('/varOne-web/rest/varone/timer')
                                   .set('Accept', 'application/json');
     const result = JSON.parse(response.text);
     this.dispatch(result);
